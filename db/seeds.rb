2f2a79545bdff111238fbfad095837d004d7c73d
Daniel Gomez Sierra <danielgomezsierra@gmail.com>
2011-12-09 16:00:01 -0500

    Task C complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,25 @@
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
 
 Product.delete_all
-Product.create(:title => 'Programming Ruby 1.9' ,
-:description =>
+Product.create(
+               :title => 'Programming Ruby 1.9' ,
+               :description =>
                %{<p>
-Ruby is the fastest growing and most exciting dynamic language out
-there. If you need to get working programs delivered fast, you should
-add Ruby to your toolbox.
-</p>},
-:image_url => '/images/ruby.jpg' ,
-:price => 49.50)
+                   Ruby is the fastest growing and most exciting dynamic language out
+                   there. If you need to get working programs delivered fast, you should
+                   add Ruby to your toolbox.
+               </p>},
+               :image_url => 'ruby.jpg' ,
+               :price => 49.50)
+
+Product.create(
+               :title => 'Debug it!',
+               :description => 'This is a debuger book.',
+               :image_url => 'debug.jpg',
+               :price => 56.00)
+
+Product.create(
+               :title => 'Rails!',
+               :description => 'This is a rails book.',
+               :image_url => 'rails.png',
+               :price => 100.00)
